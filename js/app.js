fc2dfff5cd0a6f237551c3c468e45dccf3b29a8f
Gaspar <andre.gaspar@sap.com>
2018-03-22 20:16:10 -0300

    adicionando tela e chat

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
+//função do login
+function Esconder(selected){
+    var display = document.getElementById(selected).style.display;
+    if(display == "none")
+        document.getElementById(selected).style.display = 'block';
+    else
+        document.getElementById(selected).style.display = 'none';
+}
+
+// outra fita
 $(function() {    
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
-});
-
-var nav = document.getElementById("nav");
-var showMenu = document.getElementById("showMenu");
-var hideMenu = document.getElementById("hideMenu");
-showMenu.addEventListener("click", function () {
-  nav.classList.add("show");
-});
-
-hideMenu.addEventListener("click", function () {
-  nav.classList.remove("show");
 });
